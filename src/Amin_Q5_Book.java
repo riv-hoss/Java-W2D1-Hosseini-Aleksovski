287bd37383c0e9983932f69f76a180fedebe97ff
Amin Hosseini <amin.hs117@gmail.com>
2020-07-06 13:53:15 +0200

    3rd and final push Q5, Amin.

--- src/Amin_Q5_Book.java
+++ src/Amin_Q5_Book.java
@@ -7,26 +7,40 @@ public class Amin_Q5_Book {
         String publisher;
         int page;
         int year;
+        boolean isAvailable;
 
         // constructor
         public Amin_Q5_Book(String name, String authorName, String genre,
-                    String publisher, int page, int year  ) {
+                    String publisher, int page, int year, boolean isAvaialble  ) {
             this.name = name;
             this.authorName = authorName;
             this.genre = genre;
             this.publisher = publisher;
             this.page = page;
             this.year = year;
+            this.isAvailable = isAvaialble;
 
         }
 
 
-        // method - "getter". Should be "public"
-        public void numberOfPages () {
-            System.out.printf("The book, %s, has %d pages.", this.name,this.page);
+        // method number of pages- "getter". Should be "public"
+        public void getBookInfo () {
+            System.out.printf("%s is a %s book written by %s which was published" +
+                    " by %s in %d.\n", name, genre, authorName, publisher, year);
         }
 
 
+
+        // method book availability.
+        public void isBookAvailable () {
+            if (this.isAvailable == true) {
+                System.out.printf("The book, %s, is available.\n", name);
+            } else {
+                System.out.printf("The book, %s, is currently unavailable!\n", name);
+
+            }
+
+        }
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,45 @@
 public class Main {
 
     public static void main(String[] args) {
-        Amin_Q5_Book book1 = new Amin_Q5_Book("Everything Store","Brad Stone",
-                "Non-fiction","Little",384,2013);
-        book1.numberOfPages();
+        Amin_Q5_Book bookOne = new Amin_Q5_Book("Everything Store","Brad Stone",
+                "non-fiction","Little",384,2013, true);
+
+        Amin_Q5_Book bookTwo = new Amin_Q5_Book("Shoe Dog","Phil Knight",
+                "memoir","Simon & Schuster",386,2016, true);
+
+        Amin_Q5_Book bookThree = new Amin_Q5_Book("Elon Musk","Ashlee Vance",
+                "biography","Barns",384,2015, false);
+
+        Amin_Q5_Book bookFour = new Amin_Q5_Book("The Subtle Art of Not Giving a F*ck",
+                "Mark Manson", "self-help",
+                "HarperOne",224,2013, true);
+
+        Amin_Q5_Book bookFive = new Amin_Q5_Book("The Hitchhiker's Guide to the Galaxy",
+                "Douglas Adams", "novel","PubPub",
+                120,1980, false);
+
+
+        System.out.println("\n------Part 1 - print book information------\n");
+        bookFive.getBookInfo();
+
+        System.out.println("\n------Part 2 - check if the book is available------\n");
+        bookFour.isBookAvailable();
+
+        // part 3 - store books in an array
+        System.out.println("\n------Part 4 - Store books in an array " +
+                "and loop through them.------\n");
+        Amin_Q5_Book[] myBooks = {bookOne, bookTwo, bookThree, bookFour, bookFive};
+
+        for(Amin_Q5_Book book : myBooks ) {
+            book.getBookInfo();
+            System.out.println();
+
+        }
+
+        System.out.println("\n=====================================END OF A5========" +
+                "=============================\n");
+
     }
+
+
 }
