eb4d19fabef5de1c58f28e5cef6989f4357b9708
Amin Hosseini <amin.hs117@gmail.com>
2020-07-06 10:17:44 +0200

    Initial commit

---
+++ src/Amin_Q5_Book.java
@@ -0,0 +1,33 @@
+public class Amin_Q5_Book {
+
+        // possible fields
+        String name;
+        String authorName;
+        String genre;
+        String publisher;
+        int page;
+        int year;
+
+        // constructor
+        public Amin_Q5_Book(String name, String authorName, String genre,
+                    String publisher, int page, int year  ) {
+            this.name = name;
+            this.authorName = authorName;
+            this.genre = genre;
+            this.publisher = publisher;
+            this.page = page;
+            this.year = year;
+
+        }
+
+
+        // method
+        static void numberOfPages () {
+            System.out.println();
+        }
+
+
+
+    }
+
+
